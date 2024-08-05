9171dcecf8bcc3ee78df61abfab566a6fe92e82b
Patrick Stewart <pstewart@nogac.org>
2024-08-04 17:38:53 -0700

    update(conduit): refactor from common to openapi

---
+++ packages/openapi/lib/documentable.dart
@@ -0,0 +1,20 @@
+/*
+ * This file is part of the Protevus Platform.
+ *
+ * (C) Protevus <developers@protevus.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/// This library exports the 'documentable' module from the Protevus OpenAPI package.
+///
+/// It provides access to the documentable-related functionality defined in the
+/// 'src/documentable.dart' file of the 'protevus_openapi' package.
+///
+/// This library is useful for working with documentable objects within the
+/// Protevus platform, allowing developers to utilize the pre-defined
+/// documentable-related features and structures.
+library documentable;
+
+export 'package:protevus_openapi/src/documentable.dart';
