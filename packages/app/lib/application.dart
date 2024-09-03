fe9ccd7d7c015f5a77d03ba261ef4ffb0d9d8937
Patrick Stewart <pstewart@nogac.org>
2024-09-03 13:14:31 -0700

    add(conduit): refactoring conduit core

---
+++ packages/app/lib/application.dart
@@ -0,0 +1,18 @@
+/*
+ * This file is part of the Protevus Platform.
+ *
+ * (C) Protevus <developers@protevus.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+library;
+
+export 'src/application.dart';
+export 'src/application_server.dart';
+export 'src/channel.dart';
+export 'src/isolate_application_server.dart';
+export 'src/isolate_supervisor.dart';
+export 'src/options.dart';
+export 'src/starter.dart';
