cb16079d149f7b6ffaa2d9c14f25cf938a4c03d1
Patrick Stewart <pstewart@nogac.org>
2024-11-11 12:47:52 -0700

    refactored: refactored exceptions package to support

--- packages/support/example/exception_example.dart
+++ packages/support/example/exception_example.dart
@@ -1,4 +1,4 @@
-import 'package:platform_exceptions/http_exception.dart';
+import 'package:platform_support/src/exceptions/http_exception.dart';
 
 void main() =>
     throw PlatformHttpException.notFound(message: "Can't find that page!");
---
+++ packages/support/lib/exceptions.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/exceptions/http_exception.dart';
+
+// TODO: Export any libraries intended for clients of this package.
