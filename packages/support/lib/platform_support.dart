7ec91d793cdb50734daa731d7cdb0dff794eb1ed
Patrick Stewart <pstewart@nogac.org>
2024-12-04 08:59:27 -0700

    add: adding datefactory implementation

--- packages/support/lib/platform_support.dart
+++ packages/support/lib/platform_support.dart
@@ -2,6 +2,7 @@
 library platform_support;
 
 export 'src/carbon.dart';
+export 'src/date_factory.dart';
 export 'src/fluent.dart';
 export 'src/higher_order_tap_proxy.dart';
 export 'src/optional.dart';
