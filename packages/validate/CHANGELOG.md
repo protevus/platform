7a680c50177d9ea0ba533a5610cf0e38b12b8f19
Patrick Stewart <pstewart@nogac.org>
2024-12-15 12:34:52 -0700

    add: adding validate package

---
+++ packages/validate/CHANGELOG.md
@@ -0,0 +1,97 @@
+# Change Log
+
+## 8.2.0
+
+* Require Dart >= 3.3
+* Updated `lints` to 4.0.0
+
+## 8.1.1
+
+* Updated repository link
+
+## 8.1.0
+
+* Updated `lints` to 3.0.0
+
+## 8.0.2
+
+* Fixed incorrect mismatch message handling
+
+## 8.0.1
+
+* Fixed missing mismatch errors
+
+## 8.0.0
+
+* Require Dart >= 3.0
+
+## 7.0.1
+
+* Fixed linter warnings
+
+## 7.0.0
+
+* Require Dart >= 2.17
+
+## 6.0.0
+
+* Require Dart >= 2.16
+
+## 5.0.0
+
+* Skipped release
+
+## 4.1.0
+
+* Updated linter to `package:lints`
+* Updated `build_runner` to major version 2.1.0
+* Updated `build_web_compilers` to major version 3.2.0
+
+## 4.0.2
+
+* Updated broken links in README
+* All 7 unit test passed
+
+## 4.0.1
+
+* Updated README
+
+## 4.0.0
+
+* Migrated to support Dart >= 2.12 NNBD
+
+## 3.0.0
+
+* Migrated to work with Dart >= 2.12 Non NNBD
+
+## 2.0.2
+
+* Deduplicate error messages.
+
+## 2.0.1+1
+
+* Fix bug in the implementation of `maxLength`.
+
+## 2.0.1
+
+* Patch for updated body parsing.
+
+## 2.0.0
+
+* Finish update for Angel 2.
+
+## 2.0.0-alpha.1
+
+* Update for Angel 2.
+
+## 1.0.5-beta
+
+* Use `wrapMatcher` on explicit values instead of throwing.
+* Add async matchers.
+* Add context-aware matchers.
+
+## 1.0.4
+
+* `isNonEmptyString` trims strings.
+* `ValidationException` extends `AngelHttpException`.
+* Added `requireField` and `requireFields`.
---
+++ packages/validate/test/server_test.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:platform_foundation/core.dart';
+import 'package:platform_foundation/http.dart';
+//import 'package:angel_test/angel_test.dart';
+import 'package:angel3_validate/server.dart';
+import 'package:logging/logging.dart';
+import 'package:platform_testing/http.dart';
+import 'package:test/test.dart';
+
+final Validator echoSchema = Validator({'message*': isString});
+
+void printRecord(LogRecord rec) {
+  print('${rec.time}: ${rec.level.name}: ${rec.loggerName}: ${rec.message}');
+  if (rec.error != null) print(rec.error);
+  if (rec.stackTrace != null) print(rec.stackTrace);
+}
+
+void main() {
+  late Application app;
+  late PlatformHttp http;
+  //TestClient client;
+
+  setUp(() async {
+    app = Application();
+    http = PlatformHttp(app, useZone: false);
+
+    app.chain([validate(echoSchema)]).post('/echo',
+        (RequestContext req, res) async {
+      await req.parseBody();
+      res.write('Hello, ${req.bodyAsMap['message']}!');
+    });
+
+    app.logger = Logger('angel3')..onRecord.listen(printRecord);
+    //client = await connectTo(app);
+  });
+
+  tearDown(() async {
+    //await client.close();
+    await http.close();
+  });
+
+  group('echo', () {
+    //test('validate', () async {
+    //  var response = await client.post('/echo',
+    //      body: {'message': 'world'}, headers: {'accept': '*/*'});
+    //  print('Response: ${response.body}');
+    //  expect(response, hasStatus(200));
+    //  expect(response.body, equals('Hello, world!'));
+    //});
+
+    test('enforce', () async {
+      var rq = MockHttpRequest('POST', Uri(path: '/echo'))
+        ..headers.add('accept', '*/*')
+        ..headers.add('content-type', 'application/json')
+        ..write(json.encode({'foo': 'bar'}));
+
+      scheduleMicrotask(() async {
+        await rq.close();
+        await http.handleRequest(rq);
+      });
+
+      var responseBody = await rq.response.transform(utf8.decoder).join();
+      print('Response: $responseBody');
+      expect(rq.response.statusCode, 400);
+    });
+  });
+}
