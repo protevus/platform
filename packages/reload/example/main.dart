9351152d0bcb550648289eba67019938f2ac1331
Patrick Stewart <pstewart@nogac.org>
2025-01-09 21:02:51 -0700

    add: reload package

---
+++ packages/reload/example/main.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+import 'package:platform_reload/angel3_hot.dart';
+import 'server.dart';
+
+void main() async {
+  var hot = HotReloader(createServer, [
+    Directory('src'),
+    'server.dart',
+    // Also allowed: Platform.script,
+    Uri.parse('package:angel3_hot/angel3_hot.dart')
+  ]);
+  await hot.startServer('127.0.0.1', 3000);
+}
