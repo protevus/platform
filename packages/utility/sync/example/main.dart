a129c94cab27fb416433f8f5d33ac490e32c0355
Patrick Stewart <pstewart@nogac.org>
2025-01-12 19:32:40 -0700

    add: sync package

---
+++ packages/utility/sync/example/main.dart
@@ -0,0 +1,101 @@
+import 'dart:isolate';
+import 'package:platform_foundation/core.dart';
+import 'package:platform_foundation/http.dart';
+import 'package:platform_sync/angel3_sync.dart';
+import 'package:platform_testing/testing.dart';
+import 'package:platform_websocket/io.dart' as client;
+import 'package:platform_websocket/server.dart';
+import 'package:belatuk_pub_sub/isolate.dart' as pub_sub;
+import 'package:belatuk_pub_sub/belatuk_pub_sub.dart' as pub_sub;
+import 'package:test/test.dart';
+
+void main() {
+  late Application app1, app2;
+  late TestClient app1Client;
+  late client.WebSockets app2Client;
+  late pub_sub.Server server;
+  late ReceivePort app1Port, app2Port;
+
+  setUp(() async {
+    var adapter = pub_sub.IsolateAdapter();
+
+    server = pub_sub.Server([
+      adapter,
+    ])
+      ..registerClient(const pub_sub.ClientInfo('angel_sync1'))
+      ..registerClient(const pub_sub.ClientInfo('angel_sync2'))
+      ..start();
+
+    app1 = Application();
+    app2 = Application();
+
+    app1.post('/message', (req, res) async {
+      // Manually broadcast. Even though app1 has no clients, it *should*
+      // propagate to app2.
+      var ws = req.container!.make<AngelWebSocket>();
+
+      //var body = await req.parseBody();
+      var body = {};
+      await ws.batchEvent(WebSocketEvent(
+        eventName: 'message',
+        data: body['message'],
+      ));
+      return 'Sent: ${body['message']}';
+    });
+
+    app1Port = ReceivePort();
+    var ws1 = AngelWebSocket(
+      app1,
+      synchronizationChannel: PubSubSynchronizationChannel(
+        pub_sub.IsolateClient('angel_sync1', adapter.receivePort.sendPort),
+      ),
+    );
+    await app1.configure(ws1.configureServer);
+    app1.get('/ws', ws1.handleRequest);
+    app1Client = await connectTo(app1);
+
+    app2Port = ReceivePort();
+    var ws2 = AngelWebSocket(
+      app2,
+      synchronizationChannel: PubSubSynchronizationChannel(
+        pub_sub.IsolateClient('angel_sync2', adapter.receivePort.sendPort),
+      ),
+    );
+    await app2.configure(ws2.configureServer);
+    app2.get('/ws', ws2.handleRequest);
+
+    var http = PlatformHttp(app2);
+    await http.startServer();
+    var wsPath =
+        http.uri.replace(scheme: 'ws', path: '/ws').removeFragment().toString();
+    print(wsPath);
+    app2Client = client.WebSockets(wsPath);
+    await app2Client.connect();
+  });
+
+  tearDown(() {
+    server.close();
+    app1Port.close();
+    app2Port.close();
+    app1.close();
+    app2.close();
+    app1Client.close();
+    app2Client.close();
+  });
+
+  test('events propagate', () async {
+    // The point of this test is that neither app1 nor app2
+    // is aware that the other even exists.
+    //
+    // Regardless, a WebSocket event broadcast in app1 will be
+    // broadcast by app2 as well.
+
+    var stream = app2Client.on['message'];
+    var response = await app1Client
+        .post(Uri.parse('/message'), body: {'message': 'Hello, world!'});
+    print('app1 response: ${response.body}');
+
+    var msg = await stream.first.timeout(const Duration(seconds: 5));
+    print('app2 got message: ${msg.data}');
+  });
+}
---
+++ packages/utility/sync/lib/angel3_sync.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+import 'package:platform_websocket/platform_websocket.dart';
+import 'package:belatuk_pub_sub/belatuk_pub_sub.dart' as pub_sub;
+import 'package:stream_channel/stream_channel.dart';
+
+/// Synchronizes WebSockets using `package:pub_sub`.
+class PubSubSynchronizationChannel extends StreamChannelMixin<WebSocketEvent> {
+  /// The event name used to synchronize events on the server.
+  static const String eventName = 'angel_sync::event';
+
+  final StreamChannelController<WebSocketEvent> _ctrl =
+      StreamChannelController<WebSocketEvent>();
+
+  pub_sub.ClientSubscription? _subscription;
+
+  final pub_sub.Client client;
+
+  PubSubSynchronizationChannel(this.client) {
+    _ctrl.local.stream.listen((e) {
+      client
+          .publish(eventName, e.toJson())
+          .catchError(_ctrl.local.sink.addError);
+    });
+
+    client.subscribe(eventName).then((sub) {
+      _subscription = sub
+        ..listen((data) {
+          // Incoming is a Map
+          if (data is Map) {
+            var e = WebSocketEvent.fromJson(data);
+            _ctrl.local.sink.add(e);
+          }
+        }, onError: _ctrl.local.sink.addError);
+    }).catchError((error) {
+      _ctrl.local.sink.addError(error as Object);
+    });
+  }
+
+  @override
+  Stream<WebSocketEvent> get stream => _ctrl.foreign.stream;
+
+  @override
+  StreamSink<WebSocketEvent> get sink => _ctrl.foreign.sink;
+
+  Future close() {
+    if (_subscription != null) {
+      _subscription!.unsubscribe().then((_) => client.close());
+    } else {
+      client.close();
+    }
+    return _ctrl.local.sink.close();
+  }
+}
