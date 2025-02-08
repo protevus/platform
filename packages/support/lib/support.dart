736023ec061f26bbe54561a3d718429dbe07bd24
Patrick Stewart <pstewart@nogac.org>
2025-02-08 10:10:34 -0700

    refactor: foundation package

--- packages/support/lib/support.dart
+++ packages/support/lib/support.dart
@@ -4,5 +4,14 @@ export 'src/hash.dart';
 export 'src/json.dart';
 export 'src/utils.dart';
 
-//extensions
+// Extensions
 export 'src/extensions/num.dart';
+
+/// Exceptions
+export 'src/exceptions/internal_error_exception.dart';
+export 'src/exceptions/not_found_exception.dart';
+export 'src/exceptions/query_exception.dart';
+export 'src/exceptions/validation_exception.dart';
+
+/// Service
+export 'src/service.dart';
