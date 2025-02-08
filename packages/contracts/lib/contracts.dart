abbefbdbc33f9ce70ccccb4e7e484f210ab81f23
Patrick Stewart <pstewart@nogac.org>
2025-02-08 10:10:06 -0700

    refactor: foundation package

--- packages/contracts/lib/contracts.dart
+++ packages/contracts/lib/contracts.dart
@@ -16,6 +16,9 @@ export 'src/http/request_interface.dart';
 /// Middleware
 export 'src/middleware/middleware_interface.dart';
 
+/// Routing
+export 'src/routing/router.dart';
+
 /// Storage
 export 'src/storage/storage_driver_interface.dart';
 
