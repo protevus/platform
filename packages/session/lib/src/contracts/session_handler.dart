efc74951c83daa81c4da1473cee9dd29ea808c76
Patrick Stewart <pstewart@nogac.org>
2025-02-12 13:44:18 -0700

    update: session package

--- packages/session/lib/src/contracts/session_handler.dart
+++ packages/session/lib/src/contracts/session_handler.dart
@@ -5,29 +5,28 @@ import 'package:illuminate_http/http.dart';
 abstract class SessionHandler {
   /// Starts the session for the given request.
   /// Returns the session ID.
-  Future<String> start(
-      HttpRequestContext request, HttpResponseContext response);
+  Future<String> start(Request request, Response response);
 
   /// Gets the session ID from the request.
-  String? getId(HttpRequestContext request);
+  String? getId(Request request);
 
   /// Validates that the session ID is in a valid format.
   bool isValidId(String? id);
 
   /// Sets the session ID on the response.
   Future<void> setId(
-    HttpRequestContext request,
-    HttpResponseContext response,
+    Request request,
+    Response response,
     String id,
   );
 
   /// Blocks access to the session.
-  Future<void> block(HttpRequestContext request, HttpResponseContext response);
+  Future<void> block(Request request, Response response);
 
   /// Regenerates the session ID.
   Future<String> regenerate(
-    HttpRequestContext request,
-    HttpResponseContext response, {
+    Request request,
+    Response response, {
     bool destroy = false,
   });
 }
