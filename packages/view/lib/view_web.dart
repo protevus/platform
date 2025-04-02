e8d8b40a895c603581a5f56ed71c15b5856bcd7a
Patrick Stewart <pstewart@nogac.org>
2025-04-01 22:37:34 -0700

    feat: experimental spa/web

---
+++ packages/view/lib/view_web.dart
@@ -0,0 +1,6 @@
+export 'src/web/builder_node.dart';
+export 'src/web/component.dart';
+export 'src/web/dom_builder.dart';
+export 'src/web/dom_node.dart';
+export 'src/web/fn.dart';
+export 'src/web/jael_component.dart';
